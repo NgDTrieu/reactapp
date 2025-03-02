aa7c1e9b1f37626f0cd7aac39331df08ca7a02e5
NG.D.Trieu <nguyenductrieu.toank30chl@gmail.com>
2025-03-02 20:44:20 +0700

    restructure and example of class component

---
+++ src/views/examples/MyComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+
+    render() {
+        return (
+            <h1>Xin chao, day la test vi du ve class component</h1>
+        )
+    }
+}
+
+export default MyComponent;
