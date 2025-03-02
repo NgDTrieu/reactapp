9dba4fd394422ffe06363484122dbeb15e5fd489
NG.D.Trieu <nguyenductrieu.toank30chl@gmail.com>
2025-03-02 22:23:55 +0700

    add some event

--- src/views/examples/MyComponent.js
+++ src/views/examples/MyComponent.js
@@ -2,9 +2,40 @@ import React from "react";
 
 class MyComponent extends React.Component {
 
+    state = {
+        name: 'Trieu',
+        age: 21,
+    }
+
+    handleClickButton = () => {
+        console.log('you have hit the button')
+        alert('click me')
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
     render() {
         return (
-            <h1>Xin chao, day la test vi du ve class component</h1>
+            <>
+                <div className=" first">
+                    {console.log('My name is: ', this.state.name)}
+                    Xin chao, day la test vi du ve class component cua {this.state.name}
+                </div>
+                <div className="second">
+                    Day la mot cai div khac
+                </div>
+                <div className='third'>
+                    <button onClick={() => this.handleClickButton()}>click me</button>
+                </div>
+                <div>
+                    <input type="text"
+                        onChange={(event) => { this.handleChange(event) }}
+                    />
+                </div>
+            </>
         )
     }
 }
